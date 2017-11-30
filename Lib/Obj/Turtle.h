b7ed0c5c7f6fdbc7881ca6a282171b25736258ce
Oleg N. Cher <allot@bk.ru>
2017-11-30 12:38:57 +0200

    * Lib/Turtle + examples.

---
+++ Lib/Obj/Turtle.h
@@ -0,0 +1,26 @@
+/* Ofront+ 1.0 -s */
+
+#ifndef Turtle__h
+#define Turtle__h
+
+#include "SYSTEM.h"
+
+
+import REAL Turtle_x, Turtle_y;
+import INTEGER Turtle_Black, Turtle_Blue, Turtle_Green, Turtle_Cyan, Turtle_Red, Turtle_Magenta, Turtle_Brown, Turtle_LightGray, Turtle_DarkGray, Turtle_LightBlue, Turtle_LightGreen, Turtle_LightCyan, Turtle_LightRed, Turtle_LightMagenta, Turtle_Yellow, Turtle_White, Turtle_Orange;
+
+
+import void Turtle_Back (REAL distance);
+import void Turtle_Forward (REAL distance);
+import void Turtle_Home (void);
+import void Turtle_Left (REAL a);
+import void Turtle_PenDown (void);
+import void Turtle_PenUp (void);
+import void Turtle_Quit (void);
+import void Turtle_Right (REAL a);
+import void Turtle_SetColor (INTEGER col);
+import void Turtle_ShowTurtle (void);
+import void *Turtle__init(void);
+
+
+#endif
